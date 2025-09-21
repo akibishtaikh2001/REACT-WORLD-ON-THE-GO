2227f53423be615ad17d4851322a2cc3e2a1f76b
akibishtaikh2001 <akibishtiakh2220@gmail.com>
2025-09-21 17:13:14 +0600

    Use style in react and 3 column layout

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,17 +1,20 @@
 import React, { use } from 'react';
 import Country from '../Country/Country';
+import './Countries.css'
 
 const Countries = ({ countriesPromise }) => {
 
     const countries = use(countriesPromise);
-    console.log(countries);
+    
 
     return (
         <div>
             <h1>Traveling Countries: {countries.length} </h1>
-            {
+           <div className='countries'>
+             {
                 countries.map(country => <Country key={country.ccn3} country={country} ></Country>)
             }
+           </div>
         </div>
     );
 };
